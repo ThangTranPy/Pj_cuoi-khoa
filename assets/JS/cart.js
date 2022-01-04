ccd178bd8bc7007423ebaea07d035943e27418fb
ThangTranPy <thang62232000@gmail.com>
2022-01-04 15:55:16 +0700

    uploat

---
+++ assets/JS/cart.js
@@ -0,0 +1,11 @@
+// const btn_add_cart = $.getElementById("#btn-add-cart")
+// console.log(btn_add_cart)
+
+// btn_add_cart.forEach(function(btn,index){
+//     btn.addEventListener("click",function(event){{
+//         var btnItem = event.target
+//         var product = btnItem.parentElement
+//         var productImg = product.querySelector("img").src
+//         console.log(productImg)
+//     }})
+// })
